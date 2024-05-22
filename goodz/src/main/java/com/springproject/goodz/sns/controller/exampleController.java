54f76e73b4f9f576237686482854fa54e72f70c0
parkes811 parkes811@naver.com <parkes811 parkes811@naver.com>
2024-05-22 13:06:44 +0900

    다시 세팅

---
+++ goodz/src/main/java/com/springproject/goodz/sns/controller/exampleController.java
@@ -0,0 +1,14 @@
+package com.springproject.goodz.sns.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller("/sns")
+public class exampleController {
+
+    @GetMapping("/{page}")
+    public String controller(@PathVariable("page") String page) {
+            return "/sns/" + page;
+    }
+}
