52a33aa18c7f7ca4e670b34ef2a934ce24d80a59
parkes811 parkes811@naver.com <parkes811 parkes811@naver.com>
2024-05-22 17:39:18 +0900

    front : footer, header 기능 개선

--- src/main/java/com/springproject/goodz/sns/controller/exampleController.java
+++ src/main/java/com/springproject/goodz/sns/controller/exampleController.java
@@ -3,10 +3,8 @@ package com.springproject.goodz.sns.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/sns")
+@Controller("/sns")
 public class exampleController {
 
     @GetMapping("/{page}")
