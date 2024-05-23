470bfe27457f37e4ba79ec2f99cb8024dcd17311
DoheeKim <ehgml7184@naver.com>
2024-05-23 11:59:54 +0900

    Squashed commit of the following:
    commit f7142e5b4a9efa6a6ee77a0a7c42376e3ccae79a
    Merge: fb7d6b2 ea50166
    Author: 박은서 <75926505+parkes811-inu@users.noreply.github.com>
    Date:   Thu May 23 09:44:52 2024 +0900
    
        Merge pull request #12 from parkes811-inu/pes
    
        update : src 폴더 삭제
    
    commit ea501667d8fc29ed920cc936355a14cd4b5396d4
    Author: parkes811 parkes811@naver.com <parkes811 parkes811@naver.com>
    Date:   Thu May 23 09:43:46 2024 +0900
    
        update : src 폴더 삭제
    
    commit 7ade2ffe8498450fc3a2ec4fc1e51deac6670a17
    Author: parkes811 <parkes811@naver.com>
    Date:   Wed May 22 20:42:52 2024 +0900
    
        front : favicon 추가
    
    commit fb7d6b2d8fc52106be6e531491cb6add0c92f04d
    Merge: 02ea17c 7e0261a
    Author: Nayeoonn <107173330+Nayeoonn@users.noreply.github.com>
    Date:   Wed May 22 19:56:17 2024 +0900
    
        Merge branch 'yeon' into main
    
    commit 7e0261a366c90d5e2f8712d5dff8e05da204799f
    Author: Nayeoonn <wsdg2345@naver.com>
    Date:   Wed May 22 18:27:40 2024 +0900
    
        front : pay의 index -> 미완성
    
    commit 02ea17ce37c4f182b3806a26860b817e62d4be0b
    Merge: ff0aa5f 067980c
    Author: 박은서 <75926505+parkes811-inu@users.noreply.github.com>
    Date:   Wed May 22 18:25:08 2024 +0900
    
        Merge pull request #9 from parkes811-inu/pes
    
        front : footer 이용정책 화면 추가 👍
    
    commit 067980ccff6ae3cb53eb442c593ef8838cb2fc66
    Author: parkes811 parkes811@naver.com <parkes811 parkes811@naver.com>
    Date:   Wed May 22 18:22:18 2024 +0900
    
        front : 이용정책 x 버튼 추가
    
    commit bf65a0de2129618a09533c1716617c9e3a297860
    Author: parkes811 parkes811@naver.com <parkes811 parkes811@naver.com>
    Date:   Wed May 22 18:20:17 2024 +0900
    
        front : footer 이용정책 추가
    
    commit ff0aa5f7a50dff2f2ad5f459ae2da07ab4813e11
    Merge: 29dca8a 52a33aa
    Author: 박은서 <75926505+parkes811-inu@users.noreply.github.com>
    Date:   Wed May 22 17:39:44 2024 +0900
    
        Merge pull request #8 from parkes811-inu/pes
    
        front : footer, header 기능 개선
    
    commit 52a33aa18c7f7ca4e670b34ef2a934ce24d80a59
    Author: parkes811 parkes811@naver.com <parkes811 parkes811@naver.com>
    Date:   Wed May 22 17:39:18 2024 +0900
    
        front : footer, header 기능 개선
    
    commit dcdbc486a8cc4516224432464d6e2d2fc85c2b0f
    Author: Nayeoonn <wsdg2345@naver.com>
    Date:   Wed May 22 17:20:51 2024 +0900
    
        DDL 추가
    
    commit 3c8602cb3f5acacb9578493d09c05311447fea25
    Merge: 29dca8a 5d8fa23
    Author: Nayeoonn <wsdg2345@naver.com>
    Date:   Wed May 22 16:39:43 2024 +0900
    
        Merge branch 'yeon' of https://github.com/parkes811-inu/GoodZ into yeon
    
    commit 5d8fa23ed7e71c26a6e4a6f967118a7f46ae7119
    Author: Nayeoonn <wsdg2345@naver.com>
    Date:   Wed May 22 16:20:39 2024 +0900
    
        123

---
+++ goodz/SQL/DDL.sql
@@ -0,0 +1,241 @@
+﻿-- User 테이블
+CREATE TABLE `User` (
+	`user_id`				VARCHAR(100)	NOT NULL,
+	`username`				VARCHAR(50)		NOT NULL,
+	`password`				VARCHAR(100)	NOT NULL,
+	`birth`					VARCHAR(50)		NOT NULL,
+	`phone_number`			VARCHAR(20)		NOT NULL,
+	`role`					ENUM('admin', 'user')	NOT NULL,
+	`profile_picture_url`	VARCHAR(255),
+	`account`				VARCHAR(255),
+	`created_at`			timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` 			timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id)
+) COMMENT='유저';
+
+
+
+-- Persistent_Loing 테이블
+CREATE TABLE `Persistent_Login` (
+	`persistent_no`		INT				NOT NULL AUTO_INCREMENT,
+	`user_id`			VARCHAR(100)	NOT NULL,
+	`token`				VARCHAR(255)	NOT NULL,
+	`expiration_date`	DATE			NOT NULL,
+	`created_at`		timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`state`	ENUM('remember', 'auto', 'all')	NOT NULL,
+    PRIMARY KEY (persistent_no),
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
+) COMMENT='자동로그인';
+
+
+-- Social_Login 테이블
+CREATE TABLE `Social_Login` (
+	`social_login_id`	VARCHAR(100)	NOT NULL,
+	`user_id`			VARCHAR(100)	NOT NULL,
+	`provider`			VARCHAR(50)		NOT NULL,
+	`provider_user_id`	VARCHAR(100)	NOT NULL,
+	`created_at` 		timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (social_login_id),
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
+) COMMENT='소셜로그인';
+
+-- Following 테이블
+CREATE TABLE `Following` (
+	`following_no`	INT				NOT NULL AUTO_INCREMENT,
+	`user_id`		VARCHAR(100)	NOT NULL,
+	`following_id`	VARCHAR(100),
+    PRIMARY KEY (following_no),
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
+) COMMENT='팔로잉';
+
+
+-- Follower 테이블
+CREATE TABLE `Follower` (
+	`follower_no`	INT				NOT NULL AUTO_INCREMENT,
+	`user_id`		VARCHAR(100)	NOT NULL,
+	`follower_id`	VARCHAR(100),
+    PRIMARY KEY (follower_no),
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
+) COMMENT='팔로워';
+
+
+
+-- Post 테이블
+CREATE TABLE `Post` (
+	`post_no`	INT				NOT NULL AUTO_INCREMENT,
+	`user_id`	VARCHAR(100)	NOT NULL,
+	`title`		VARCHAR(100) 	NOT NULL,
+	`content`	TEXT,
+	`image_url`	VARCHAR(255)	NOT NULL,
+	`created_at` timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` timestamp		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (post_no),
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
+) COMMENT='게시글';
+
+
+
+-- Comment 테이블
+CREATE TABLE `Comment` (
+	`c_no`			INT				NOT NULL AUTO_INCREMENT,
+	`post_no`		INT				NOT NULL,
+	`user_id`		VARCHAR(100)	NOT NULL,
+	`comment`		TEXT,
+	`created_at`	 timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`	 timestamp		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (c_no),
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (post_no) REFERENCES Post(post_no)
+) COMMENT='댓글';
+
+
+-- Like 테이블
+CREATE TABLE `Like` (
+	`like_no`	INT				NOT NULL AUTO_INCREMENT,
+	`c_no`		INT				NOT NULL,
+	`user_id`	VARCHAR(100)	NOT NULL,
+	`post_no`	INT				NOT NULL,
+	`created_at`	 timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`	 timestamp		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (like_no),
+    FOREIGN KEY (c_no) REFERENCES Comment(c_no),
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (post_no) REFERENCES Post(post_no)
+);
+
+
+-- Tag 테이블
+CREATE TABLE `Tag` (
+	`t_no`		INT	NOT NULL AUTO_INCREMENT,
+	`p_no`		INT	NOT NULL,
+	`post_no`	INT	NOT NULL,
+    PRIMARY KEY (t_no),
+    FOREIGN KEY (p_no) REFERENCES Product(p_no),
+    FOREIGN KEY (post_no) REFERENCES Post(post_no)
+) COMMENT='상품 태그';
+
+
+-- Product 테이블
+CREATE TABLE `Product` (
+	`p_no`				INT				NOT NULL AUTO_INCREMENT,
+	`product_name`		VARCHAR(100)	NOT NULL,
+	`description`		TEXT,
+	`price`				INT				NOT NULL,
+	`brand`				VARCHAR(50)		NOT NULL,
+	`category`			VARCHAR(50)		NOT NULL,
+	`size`				VARCHAR(100)	NOT NULL,
+	`views`				INT				NOT NULL DEFAULT '0',
+	`stock_quantity`	INT				NOT NULL,
+	`image_url`			VARCHAR(255)	NOT NULL,
+	`created_at`	    timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`	 	timestamp		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     PRIMARY KEY (p_no)
+) COMMENT='상품';
+
+
+
+
+-- Pricehistory 테이블
+CREATE TABLE `Pricehistory` (
+	`price_history_no`	INT				NOT NULL AUTO_INCREMENT,
+	`p_no`				INT				NOT NULL,
+	`fluctuated_price`	INT				NOT NULL,
+	`created_at`	    timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`	 	timestamp		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (price_history_no),
+    FOREIGN KEY (p_no) REFERENCES Product(p_no)
+) COMMENT='가격변동';
+
+
+
+
+-- Wishlist 테이블
+CREATE TABLE `Wishlist` (
+	`w_no`		INT				NOT NULL AUTO_INCREMENT,
+	`user_id`	VARCHAR(100)	NOT NULL,
+	`p_no`		INT				NOT NULL,
+	`created_at` timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` timestamp		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (w_no),
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (p_no) REFERENCES Product(p_no)
+) COMMENT='관심';
+
+
+-- Sales 테이블
+CREATE TABLE `Sales` (
+	`s_no`			INT				NOT NULL AUTO_INCREMENT,
+	`user_id`		VARCHAR(100)	NOT NULL,
+	`p_no`			INT				NOT NULL,
+    `sale_tracking_no` VARCHAR(50)	NOT NULL,
+	`sale_price`	INT				NOT NULL,
+	`sale_state`	ENUM('pending', 'completed', 'cancelled')	NOT NULL,
+	`sale_date`		timestamp		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (s_no),
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (p_no) REFERENCES Product(p_no)
+) COMMENT='판매';
+
+
+-- Inspection 테이블
+CREATE TABLE `Inspection` (
+	`i_no`		INT	NOT NULL AUTO_INCREMENT,
+	`s_no`		INT	NOT NULL,
+	`ins_state`	ENUM('pending', 'verified', 'rejected')	NOT NULL,
+	`ins_date`	timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (i_no),
+    FOREIGN KEY (s_no) REFERENCES Sales(s_no)
+)COMMENT='검수';
+
+
+
+-- Pusrchase 테이블
+CREATE TABLE `Purchase` (
+	`purchase_no`		INT				NOT NULL AUTO_INCREMENT,
+	`user_id`			VARCHAR(100)	NOT NULL,
+	`p_no`				INT				NOT NULL,
+	`purcahse_pirce`	INT				NOT NULL,
+	`payment_method`	VARCHAR(50)		NOT NULL,
+	`purchase_state`	ENUM('pending', 'shipped', 'delivered', 'cancelled')	NOT NULL,
+	`purchase_date`		timestamp		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (purchase_no),
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (p_no) REFERENCES Product(p_no)
+)COMMENT='구매';
+
+
+
+
+-- Shipment 테이블
+CREATE TABLE `Shipment` (
+	`shipment_no`	INT				NOT NULL AUTO_INCREMENT,
+	`purchase_no`	INT	 			NOT NULL,
+	`user_id`		VARCHAR(100)	NOT NULL,
+	`p_no`			INT				NOT NULL,
+	`tracking_no`	VARCHAR(50)		NOT NULL,
+	`shipment_state`	ENUM('pending', 'shipped', 'in_transit', 'delivered', 'returned')	NOT NULL,
+	`shipped_date`		timestamp	NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`delivered_date`	DATE,
+    PRIMARY KEY (shipment_no),
+    FOREIGN KEY (purchase_no) REFERENCES Purchase(purchase_no),
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (p_no) REFERENCES Product(p_no)
+) COMMENT='배송';
+
+
+-- Shippingaddress
+CREATE TABLE `Shippingaddress` (
+	`address_no`		INT				NOT NULL AUTO_INCREMENT,
+	`user_id`			VARCHAR(100)	NOT NULL,
+	`recipient_name`	VARCHAR(100)	NOT NULL,
+	`address`			VARCHAR(255)	NOT NULL,
+	`zip_code`			VARCHAR(20)		NOT NULL,
+	`phone_number`		VARCHAR(20)		NOT NULL,
+	`is_default`		BOOLEAN			NOT NULL DEFAULT TRUE,	
+	`type`				ENUM('return','buy')	NOT NULL,
+	`created_at`	  	timestamp 		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`	 	timestamp		NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (address_no),
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
+) COMMENT='배송주소';
+
---
+++ goodz/src/main/java/com/springproject/goodz/controller/MainController.java
@@ -0,0 +1,53 @@
+package com.springproject.goodz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import groovy.util.logging.Slf4j;
+
+
+@Slf4j
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+ 
+    @GetMapping("/{page}")
+    public String page(@PathVariable("page") String page) {
+        return page;
+    }
+
+
+    // footer 하단 링크 
+    @GetMapping("/footer/{id}")
+    public String getFooterMapping(@PathVariable("id") int id, Model model) {
+        String template;
+        switch (id) {
+            case 1:
+                template = "common/privacy"; // templates/common/privacy.html
+                break;
+            case 2:
+                template = "common/inspection"; // templates/common/inspection.html
+                break;
+            case 3:
+                template = "common/store_info"; // templates/common/store_info.html
+                break;
+            case 4:
+                template = "common/guideLine"; // templates/common/community_guidelines.html
+                break;
+            default:
+                template = "/"; // default to home
+        }
+        return template;
+    }
+
+    // @GetMapping("/{domain}/{page}")
+    // public String page(@PathVariable("domain") String domain
+    //                   ,@PathVariable("page") String page  ) {
+    //     return domain + "/" + page;
+    // }
+    
+}
