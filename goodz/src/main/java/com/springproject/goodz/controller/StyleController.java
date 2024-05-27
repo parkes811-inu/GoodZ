a621b08ebe55479aa14fe990076a50bf5a6d9a01
DoheeKim <ehgml7184@naver.com>
2024-05-27 12:08:53 +0900

    스타일 상세 및 댓글 완료

--- goodz/src/main/java/com/springproject/goodz/controller/StyleController.java
+++ goodz/src/main/java/com/springproject/goodz/controller/StyleController.java
@@ -16,7 +16,7 @@ public class StyleController {
      * 전체 게시글 목록
      * @return
      */
-    @GetMapping("/list")
+    @GetMapping("")
     public String list() {
         return "/style/list";
     }
@@ -28,6 +28,15 @@ public class StyleController {
     @GetMapping("/read")
     public String read() {
         return "/style/read";
+    }
+
+    /**
+     * 게시글 상세 (무플버전 확인용)
+     * @return
+     */
+    @GetMapping("/cmmtX")
+    public String cmmtTest() {
+        return "/style/read_cmmtX";
     }
     
 }
