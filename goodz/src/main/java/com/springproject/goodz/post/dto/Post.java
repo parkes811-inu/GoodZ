20009bc274bdf343206e27066143a5faec9b38cc
DoheeKim <ehgml7184@naver.com>
2024-05-28 21:18:51 +0900

    5/28 커밋

---
+++ goodz/src/main/java/com/springproject/goodz/post/dto/Post.java
@@ -0,0 +1,24 @@
+package com.springproject.goodz.post.dto;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Data;
+
+@Data
+public class Post {
+    
+    private int postNo;         // 게시글 번호
+    private String userId;      // 작성자
+    private String content;     // 작성내용
+    private String imageUrl;    // 첨부이미지 url
+    private Date createdAt;     // 작성일자
+    private Date updatedAt;     // 수정일자
+
+    
+    // 파일에 대한 변수를 받기위해 정보 추가
+    List<MultipartFile> file;
+
+}
