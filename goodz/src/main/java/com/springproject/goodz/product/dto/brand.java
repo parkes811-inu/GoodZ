f3f669088855fcdcdfba6554aed5102908b29004
DoheeKim <ehgml7184@naver.com>
2024-05-29 10:22:36 +0900

    브랜드 dto 완료

---
+++ goodz/src/main/java/com/springproject/goodz/product/dto/brand.java
@@ -0,0 +1,12 @@
+package com.springproject.goodz.product.dto;
+
+import lombok.Data;
+
+@Data
+public class brand {
+    
+    private int bNo;
+    private String bName;
+    private String imgeUrl;
+
+}
