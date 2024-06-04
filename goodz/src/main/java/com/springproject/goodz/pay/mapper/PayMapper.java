e24810cd20e36bdaa4c263c5da46aff9601f5bea
Nayeoonn <wsdg2345@naver.com>
2024-06-04 20:18:46 +0900

    결제쪽 수정 필요

--- goodz/src/main/java/com/springproject/goodz/pay/mapper/PayMapper.java
+++ goodz/src/main/java/com/springproject/goodz/pay/mapper/PayMapper.java
@@ -1,5 +1,12 @@
 package com.springproject.goodz.pay.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.springproject.goodz.pay.dto.Purchase;
+
+@Mapper
 public interface PayMapper {
     
+    // 구매 등록
+    public void insertPurchase(Purchase purchase) throws Exception;
 }
