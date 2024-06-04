b767d2b91f7bf1d0be47eeec374880cd8919428e
Nayeoonn <wsdg2345@naver.com>
2024-06-04 14:44:00 +0900

    back : 상품, 브랜드 목록 페이징처리 & 검색 기능

--- goodz/src/main/java/com/springproject/goodz/product/service/BrandService.java
+++ goodz/src/main/java/com/springproject/goodz/product/service/BrandService.java
@@ -3,11 +3,18 @@ package com.springproject.goodz.product.service;
 import java.util.List;
 
 import com.springproject.goodz.product.dto.Brand;
+import com.springproject.goodz.product.dto.Page;
 
 public interface BrandService {
 
     // 브랜드 목록
     public List<Brand> list() throws Exception;
+
+    // 브랜드 목록 - 페이징 + 검색
+    public List<Brand> brandList(Page page, String keyword) throws Exception;
+
+    // 전체 데이터 개수 가져오기
+    public int getTotalCount(String keyword) throws Exception;
 
     // 브랜드 등록
     public int insert(Brand brand) throws Exception;
