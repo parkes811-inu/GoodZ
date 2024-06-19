8c2b2c1f8816eb38688698c653a5009e491dec57
DoheeKim <ehgml7184@naver.com>
2024-06-20 04:11:12 +0900

    샘플데이터 DDL 추가

---
+++ goodz/SQL/product_sample.sql
@@ -0,0 +1,29 @@
+/*
+-- Query: SELECT * FROM goodz.product
+LIMIT 0, 1000
+
+-- Date: 2024-06-20 04:05
+*/
+INSERT INTO `product` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (1,'IAB Studio','IAB Studio T-Shirt Green Pink',46000,'top',0,'2024-06-20 01:12:13','2024-06-20 01:12:13');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (2,'IAB Studio','IAB Studio 10th Anniversary T-Shirt White',46000,'top',0,'2024-06-20 01:16:30','2024-06-20 02:31:39');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (3,'Martin Kim','Matin Kim Black Bunny Toy Keyring Black',22000,'accessory',0,'2024-06-20 01:17:11','2024-06-20 02:31:39');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (4,'Martin Kim','Matin Kim Half Shirring Ribbon Round Bag Black',68000,'accessory',0,'2024-06-20 01:17:40','2024-06-20 01:17:40');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (5,'Martin Kim','Matin Kim Half Shirring Ribbon Round Bag Pink',68000,'accessory',0,'2024-06-20 01:18:10','2024-06-20 01:18:10');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (6,'Martin Kim','Matin Kim Vintage Card Wallet Black',32000,'accessory',0,'2024-06-20 01:18:44','2024-06-20 01:18:44');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (7,'Martin Kim','Matin Kim Logo Coating Jumper Pink',98000,'top',0,'2024-06-20 01:20:10','2024-06-20 02:31:39');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (8,'New Balance','New Balance 530 Silver',134000,'shoes',0,'2024-06-20 01:21:55','2024-06-20 01:21:55');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (9,'New Balance','New Balance 530 Steel Grey',98000,'shoes',0,'2024-06-20 01:22:45','2024-06-20 01:22:45');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (10,'Nike','Nike ACG Snowgrass Cargo Shorts Black',58000,'pants',1,'2024-06-20 01:23:58','2024-06-20 02:33:16');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (11,'Nike','Nike Air Force 1 \'07 Low White',129000,'shoes',0,'2024-06-20 01:24:31','2024-06-20 02:31:39');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (12,'Nike','Nike Air Force 1 \'07 LV8 Midnight Navy',165000,'shoes',0,'2024-06-20 01:25:44','2024-06-20 02:31:39');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (13,'Nike','Nike Air Force 1 \'07 WB Flax',127000,'shoes',0,'2024-06-20 01:26:16','2024-06-20 01:26:16');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (14,'Nike','Nike Cortez White and Black',87000,'shoes',0,'2024-06-20 01:26:52','2024-06-20 02:31:39');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (15,'Nike','Nike Dri-Fit Park 7 T-Shirt Black',32000,'top',0,'2024-06-20 01:27:13','2024-06-20 02:31:39');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (16,'Nike','Nike Dri-Fit Park 7 T-Shirt Volt',32000,'top',0,'2024-06-20 01:27:40','2024-06-20 01:27:40');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (17,'Nike','Nike Dri-Fit Park 7 T-Shirt White',32000,'top',1,'2024-06-20 01:28:36','2024-06-20 02:33:07');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (18,'Nike','Nike NRG Solo Swoosh Fleece Shorts Dark Grey Heather',54000,'pants',0,'2024-06-20 01:29:12','2024-06-20 01:29:12');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (19,'Nike','Nike Tempo Running Shorts Black',43000,'pants',0,'2024-06-20 01:29:40','2024-06-20 01:29:40');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (20,'Nonnod','Nonnod Side Zip Cargo Pants Beige',130000,'pants',0,'2024-06-20 01:31:11','2024-06-20 01:31:11');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (21,'Stussy','Stussy Big Stock Water Shorts Black',54000,'pants',0,'2024-06-20 01:32:00','2024-06-20 01:32:00');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (22,'Stussy','Stussy Big Stock Water Shorts Lime',54000,'pants',0,'2024-06-20 01:32:46','2024-06-20 01:32:46');
+INSERT INTO `` (`p_no`,`b_name`,`product_name`,`initial_price`,`category`,`views`,`created_at`,`updated_at`) VALUES (23,'Stussy','Stussy Overdyed Stock Logo Shorts Grey Heather',54000,'pants',0,'2024-06-20 01:33:37','2024-06-20 01:33:37');
